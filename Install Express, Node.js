55de7fb996f034e6d51e9fcc4dfc7154e0a28f5a
rb4bhushan <ravibhushan501@yahoo.com>
2015-02-10 10:09:57 +0530

    10-feb:10AM

--- Install Express, Node.js
+++ Install Express, Node.js
@@ -1,5 +1,6 @@
 
-1) – Install Node.js :
+1) - Install Node.js :
+
 // visit http://nodejs.org and click the download button.
 // Run the installer. 
 // That's it, you have installed Node.js and NPM – Node Package Manager.
@@ -7,7 +8,8 @@
  - Open a Node.js command prompt
  - cd to the directory in which you wish to keep your test apps, eg. C:\nodejs
 
-2) - Install Express 
+2) - Install Express : 
+
 //Express, which is a framework that takes Node from a barebones application and turns it into something that behaves more like the web servers
 
  C:\nodejs> npm install -g express
@@ -29,4 +31,4 @@
  
 //These commands will create an empty Express project in the directory we just created socketio-test. We then run " C:\nodejs\socketio-test>npm install " to get all the dependencies that are needed to run the app. To test the empty application, run " C:\nodejs\socketio-test>node app " then navigate your browser to http://yourDropletIp-or-domainName:3000. You should get a simple welcome message saying: "Welcome to Express".
 //If you see the welcome message then you have a basic express application ready and running!
-//Be sure to kill your VPS with the Ctrl+C keyboard command before you keep going. 
+//Ctrl+c - To quit the last node app.This is important. 
